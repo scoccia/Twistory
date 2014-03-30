adaba32d93551c4663c45b0bca0b394c657142c8
scoccia <cocciasimone@gmail.com>
2014-03-30 19:37:41 +0200

    first version

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TwittwarApp::Application.config.secret_key_base = '4256368535dd777b2f89d065f59ff222c148aa85db9e1627ae0b42b4f4ac34b3e4c1586eed9bf3dec8c7ab78bc3869c21c31ed0297455262db52a8b4a47f85a0'
