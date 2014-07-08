d1b9fb729e9f0def20f80421a68276e920419b32
davide-di-carlo <davide_di_carlo@yahoo.it>
2014-07-08 10:36:29 +0200

    Update feed.rb

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -1,20 +1,20 @@
 class Feed < ActiveRecord::Base
-	validate :checking_feed_text
-	validate :checking_for_date
+  validate :checking_feed_text
+  validate :checking_for_date
 	
-	def checking_feed_text
+  def checking_feed_text
 	  if !(feed_text.present?)
-	    errors[:base] = "Non puoi pubblicare feed vuoti!"
-	  elsif feed_text.size > 140
-	    errors[:base] = "Il feed non puo' contenere piu' di 140 caratteri!"
-	  end
-	end 
+      errors[:base] = "Non puoi pubblicare feed vuoti!"
+    elsif feed_text.size > 140
+      errors[:base] = "Il feed non puo' contenere piu' di 140 caratteri!"
+    end
+  end 
 	
-	def checking_for_date
-		if !(date.present?) 
-  		errors[:base] = "E' necessario inserire una data di pubblicazione per il feed!"
-  	elsif date.utc <= DateTime.now.utc
-  		errors[:base] = "La data non puo' essere nel passato!"
-  	end
-	end
+  def checking_for_date
+    if !(date.present?) 
+      errors[:base] = "E' necessario inserire una data di pubblicazione per il feed!"
+    elsif date.utc <= DateTime.now.utc
+      errors[:base] = "La data non puo' essere nel passato!"
+    end
+  end
 end
