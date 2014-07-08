c7349f9d59147b3195ffaa705bb38fe830c2dfe5
davide-di-carlo <davide_di_carlo@yahoo.it>
2014-07-08 10:38:43 +0200

    Update feed.rb

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -3,7 +3,7 @@ class Feed < ActiveRecord::Base
   validate :checking_for_date
 	
   def checking_feed_text
-	  if !(feed_text.present?)
+    if !(feed_text.present?)
       errors[:base] = "Non puoi pubblicare feed vuoti!"
     elsif feed_text.size > 140
       errors[:base] = "Il feed non puo' contenere piu' di 140 caratteri!"
