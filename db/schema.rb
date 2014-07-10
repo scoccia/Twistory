62f87188db97df421cace99beb01e27d4bd94041
Davide Di Carlo <davide_di_carlo@yahoo.it>
2014-07-10 16:20:17 +0200

    modified:   db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140705191336) do
+ActiveRecord::Schema.define(version: 20140710135028) do
 
   create_table "feeds", force: true do |t|
     t.string   "feed_text"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20140705191336) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
+    t.string   "feed_image"
   end
 
   create_table "users", force: true do |t|
