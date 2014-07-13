2ad066b364538a0c7ceafdee22d69c982ffdfc33
Davide Di Carlo <davide_di_carlo@yahoo.it>
2014-07-13 19:02:56 +0200

    modified:   app/models/user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,25 @@
 class User < ActiveRecord::Base
+
+  mount_uploader :profile_image, UserImageUploader
+  
+  validate :checking_for_user_name
+  validate :checking_for_image_size
+ 
+  private
+  
+  def checking_for_user_name
+    if !(user_name.present?)
+      errors[:base] = "Devi inserire un nome pubblico"
+    elsif user_name.size > 15
+      errors[:base] = "Il nome non puo' superare i 15 caretteri"
+    end
+  end 
+	
+ 
+  def checking_for_image_size
+    errors[:base] = "L'immagine non può superare 300 Kb" if profile_image.size > 300.kilobytes
+  end
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
