af908497d1016d5ee329f79188f0cf0e7b92131e
Davide Di Carlo <davide_di_carlo@yahoo.it>
2014-07-14 16:53:52 +0200

    modified:   app/controllers/application_controller.rb 	modified:   app/controllers/feeds_controller.rb

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -1,4 +1,5 @@
 class FeedsController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_feed, only: [:show, :edit, :update, :destroy]
 	
   # error message for feeds not belonging to a user
