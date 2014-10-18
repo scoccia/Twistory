7d7c770b5e73bd27d4e0ce744ee28fb85230bf4a
Davide Di Carlo <davide_di_carlo@yahoo.it>
2014-10-18 18:24:48 +0200

    modified:   app/controllers/feeds_controller.rb 	modified:   app/models/feed.rb 	modified:   app/views/feeds/_form.html.erb 	modified:   app/views/feeds/index.html.erb 	modified:   app/views/feeds/profile.html.erb 	modified:   lib/tasks/send_feeds_to_twitter.rake 	modified:   lib/tasks/translate_existing_feeds.rake 	modified:   public/acces_denied.html

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -6,11 +6,11 @@ class FeedsController < ApplicationController
   before_action :set_feed, only: [:show, :edit, :update, :destroy]
 	
   # error message for feeds not belonging to a user
-  def err_mex 
+  def error_message 
     flash.now[:notice] = "Non sei il possessore di questo feed e non detieni i privilegi per alterarlo!" 			
     render "show"
   end
-  # err_mex end #
+  #  end #
 		
   # GET /feeds
   def index
@@ -45,9 +45,9 @@ class FeedsController < ApplicationController
     @feed.date = @feed.date + 2.hour 
 
     if @feed.user_id != current_user.id
-      err_mex 
+      error_message 
     elsif @feed.has_been_published == true
-      flash[:notice] = "Non puoi modificare feed gia' pubblicati"
+      flash[:notice] = "Non puoi modificare feed gia\' pubblicati"
       render "show"
     end
 
@@ -81,10 +81,14 @@ class FeedsController < ApplicationController
       feed_text_english = translate_feed(@feed.feed_text)
       @feed.feed_text_english = feed_text_english
     end
-    
+        
     respond_to do |format|
       if @feed.save
-        flash[:notice] = 'Il Feed e\' stato creato con successo'
+        if feed_text_english.present?
+          flash[:notice] = 'Entrambe le versioni del feed sono state create con successo'
+        else
+          flash[:notice] = 'La versione Italiana del feed e\' stata creata con successo'
+        end
         format.html { redirect_to action: 'index' }
       else
         format.html { render action: 'new' }
@@ -102,9 +106,9 @@ class FeedsController < ApplicationController
         if @feed.update(feed_params)
 
           # Convert the CET/CEST time (inserted by the user) in UTC time (expected by the Database)
-    	  # TODO: have a better solution (see create action)
-    	  @feed.date = @feed.date - 2.hour 
-	  @feed.save
+    	    # TODO: have a better solution (see create action)
+    	    @feed.date = @feed.date - 2.hour 
+          @feed.save
 
           flash[:notice] = 'Il Feed e\' stato aggiornato con successo'
           format.html { redirect_to action: 'index' }
@@ -114,7 +118,7 @@ class FeedsController < ApplicationController
         end
       end
     else
-      err_mex
+      error_message
     end
   end	
   # update end #
@@ -126,9 +130,9 @@ class FeedsController < ApplicationController
       respond_to do |format|
         format.html { redirect_to feeds_url }
         format.json { head :no_content }
-        end
+      end
     else
-      err_mex
+      error_message
     end
   end	
   # destroy end #
@@ -180,7 +184,7 @@ class FeedsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def feed_params
-      params.require(:feed).permit(:feed_text, :date, :feed_image)
+      params.require(:feed).permit(:feed_text, :feed_text_english, :date, :feed_image)
     end
     # feed_params end #
   end
